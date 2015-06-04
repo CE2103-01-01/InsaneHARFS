ee08c60d41cafed3adff0a077dd6bdb14d964418
Pablo Rodriguez <paroque28@gmail.com>
2015-06-03 23:02:14 -0600

    CodeBlocks

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include "models/pointer/MemoryPointer.h"
+#include <stdio.h>
+#include "models/Pointer.h"
 
 using namespace std;
 
 int main() {
-    MemoryPointer<int> number = malloc(sizeof(int));
-    number.set(2);
-    cout << "Hello, World!" << *number.get()<<endl;
+    Pointer<int> number = malloc(sizeof(int));
+    *number = 2;
+    cout << "Hello, World!" << *number<<endl;
+    number.Free();
     return 0;
 }
