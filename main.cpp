2383caa0c4affa908ebd4ccc50fd1331f7ee5373
roberto <rbj1504@hotmail.com>
2015-06-06 10:01:30 -0600

    writer ready

--- main.cpp
+++ main.cpp
@@ -5,10 +5,20 @@
  */
 int main(int argc, char* argv[]) {
     std::string path = "proof";
-    char* text = "proof";
-    char* change = "abb";
+    char* text = "aaaaaaaaaa";
+    char* change = "bbbbb";
     int offset = 2;
-    FileManager::createFile(text,path,5);
-    FileManager::writeFile(change,path,2,3);
+    int sizeOfText = 10;
+    int sizeOfChange = 5;
+    void* completeTextReaded = malloc(sizeOfText);
+    void* changeReaded = malloc(sizeOfChange);
+    FileManager::createFile(text,path,sizeOfText);
+    FileManager::writeFile(change,path,offset,sizeOfChange);
+    FileManager::readFile(completeTextReaded, path, NULL, sizeOfText);
+    FileManager::readFile(changeReaded, path, offset, sizeOfChange);
+    for(int i = 0; i < sizeOfText; i++) std::cout << *static_cast<char*>(completeTextReaded+i);
+    std::cout << std::endl;
+    for(int i = 0; i < sizeOfChange; i++) std::cout << *static_cast<char*>(changeReaded+i);
+    std::cout << std::endl;
     return 0;
 }
