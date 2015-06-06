ab284825d2b68ce740d2770c9bb4eee53e387758
roberto <rbj1504@hotmail.com>
2015-06-06 08:33:37 -0600

    writer almost ready

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 int main(int argc, char* argv[]) {
     std::string path = "proof";
     char* text = "proof";
+    char* change = "abb";
+    int offset = 2;
     FileManager::createFile(text,path,5);
-    FileManager::writeFile("abb",path,2,3);
+    FileManager::writeFile(change,path,2,3);
     return 0;
 }
